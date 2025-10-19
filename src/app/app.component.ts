e7dbcf28767d63d966885a3aee1817aaeace3f2b
Abdourahamane10 <amdroudjae@gmail.com>
2025-10-19 16:13:14 +0200

    Création du sideNav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { SideNavComponent } from './side-nav/side-nav.component';
+
+import { MediaMatcher } from '@angular/cdk/layout';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  ViewChild,
+  inject,
+  viewChild,
+} from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    SideNavComponent,
+    MatSidenavModule,
+    MatListModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'sanaShop';
---
+++ src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,64 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, ViewChild, inject, viewChild } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { SideNavService } from '../side-nav.service';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-side-nav',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    HeaderComponent,
+  ],
+  templateUrl: './side-nav.component.html',
+  styleUrl: './side-nav.component.css',
+})
+export class SideNavComponent implements OnDestroy, AfterViewInit {
+  @ViewChild('snav') sideNav!: MatSidenav;
+  mobileQuery: MediaQueryList;
+
+  fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
+
+  // fillerContent = Array.from(
+  //   { length: 50 },
+  //   () =>
+  //     `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+  //      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+  //      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+  //      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+  //      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.` max-width: 600px
+  // );
+
+  private _mobileQueryListener: () => void;
+
+  constructor(private sideNavService: SideNavService) {
+    const changeDetectorRef = inject(ChangeDetectorRef);
+    const media = inject(MediaMatcher);
+
+    this.mobileQuery = media.matchMedia('(max-width: 539px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngAfterViewInit(): void {
+    this.sideNavService.setSideNav(this.sideNav);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  // shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(
+  //   window.location.host
+  // );
+}
